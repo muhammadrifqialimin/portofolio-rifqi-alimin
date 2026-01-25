bd76416911599db2b605b40574a6c38406f8d545
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2026-01-25 14:13:15 +0700

    initialize project with Vite and React setup

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default [
+  { ignores: ["dist"] }, // Pengganti globalIgnores untuk format baru
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      // --- PERBAIKAN DI SINI ---
+      // Kita ubah 'error' menjadi 'warn' (kuning) atau 'off' (mati)
+      "no-unused-vars": "off",
+      "react/prop-types": "off",
+    },
+  },
+];
